4b4a32783bda07bfca9548397eae2a53bd8e9336
DoomPenguin9 <yordan97723@gmail.com>
2021-12-05 09:39:57 +0000

    updated README

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-Some simple scripts which make recording gameplay with sound using ffmpeg and vaapi easier on Linux
+Some simple scripts for recording gameplay with sound on Linux using FFMPEG VAAPI and kmsgrab.
 
 # Requirements
 + An AMD GPU(R9 Fury or newer)
-+ librva-mesa-driver
++ libva-mesa-driver
 + tmux
 + ffmpeg
 
+# Usage
+By default, this is setup for 1440p@60fps capture at a bitrate of 60 000kbps, you can change the bitrate by changing `-b:v 60M` to whatever you want your bitrate to be. I recommend adding the scripts to your `$PATH` so that you can execute them from anywhere. To do this, open your .bashrc(or.zshrc if you are using zsh for your shell) and paste the folioing at the end of the file:
+
+```
+export PATH=$PATH:"/home/yordan/vaapi-game-capture-scripts:$PATH"
+alias vpcapture="recording-script.sh"
+alias vpmerge="merging.sh"
+
+```
+
+Now you can run the recording script by opening a terminal, starting `tmux`, typing `vpcapture`, and pressing enter. To stop the recording, press Ctrl + C to kill each of the 2 processes running in the terminal. Then you can navigate to directory where you saved your recordings and run `vpmerge` to put the audio and video files together and (optionally)delete the original separate files. 
--- recording-script.sh
+++ recording-script.sh
@@ -1,4 +1,5 @@
 DIRPATH="$(pwd)"
+# Input if you want to set a custom output directory. If you input n, then the files will be outputted into the directory you are running the script from
 read -p "Do you want to set a directory to write the recorded files to[y/n]:" DIR
 if [[ $DIR = y ]]
 then
@@ -6,10 +7,14 @@ then
 else
 	:
 fi
-	echo Outputting to $DIRPATH
-	tmux new-session -d
-	tmux set-option remain-on-exit off
-	tmux split-window "LIBVA_DRIVER_NAME=radeonsi ffmpeg -y -device /dev/dri/card0 -f kmsgrab -framerate 60 -vsync 0 -i - -vf 'hwmap=derive_device=vaapi,crop=2560:1440:0:0,scale_vaapi=w=2560:h=1440:format=nv12' -c:v hevc_vaapi -b:v 60M $DIRPATH/$(date +"%Y_%m_%d_%I_%M_%p").mkv"
-	echo "$(date +"%Y_%m_%d_%I_%M_%p")" > $DIRPATH/filenames.txt
-	tmux split-window "ffmpeg -y -f pulse -i default -b:a 192k $DIRPATH/$(date +"%Y_%m_%d_%I_%M_%p")-audio.opus"
-	echo "$(date +"%Y_%m_%d_%I_%M_%p")-audio" >> $DIRPATH/filenames.txt
+
+# Video capture
+echo Outputting to $DIRPATH
+tmux new-session -d
+tmux set-option remain-on-exit off
+tmux split-window "LIBVA_DRIVER_NAME=radeonsi ffmpeg -y -device /dev/dri/card0 -f kmsgrab -framerate 60 -vsync 0 -i - -vf 'hwmap=derive_device=vaapi,crop=2560:1440:0:0,scale_vaapi=w=2560:h=1440:format=nv12' -c:v hevc_vaapi -b:v 60M $DIRPATH/$(date +"%Y_%m_%d_%I_%M_%p").mkv"
+
+# Sound capture
+echo "$(date +"%Y_%m_%d_%I_%M_%p")" > $DIRPATH/filenames.txt
+tmux split-window "ffmpeg -y -f pulse -i default -b:a 192k $DIRPATH/$(date +"%Y_%m_%d_%I_%M_%p")-audio.opus"
+echo "$(date +"%Y_%m_%d_%I_%M_%p")-audio" >> $DIRPATH/filenames.txt
